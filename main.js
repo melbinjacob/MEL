ba6d6cf29bea07e8a1bcd8c91fcdecd7787f350c
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2022-09-11 19:16:40 +0530

    Changed to dark theme and added a new portion

--- main.js
+++ main.js
@@ -2,3 +2,27 @@ let parent = document.querySelectorAll('.animate-text');
 for(let i = 0; i < parent.length; i++) {
   parent[i].style.width = parent[i].children[0].clientWidth + "px"; 
 };
+
+window.addEventListener('mousemove', handleMouseMove);
+window.addEventListener('resize', handleWindowResize);
+
+const spansSlow = document.querySelectorAll('.spanSlow');
+const spansFast = document.querySelectorAll('.spanFast');
+
+let width = window.innerWidth;
+
+function handleMouseMove(e) {
+  let normalizedPosition = e.pageX / (width/2) - 1;
+  let speedSlow = 100 * normalizedPosition;
+  let speedFast = 200 * normalizedPosition;
+  spansSlow.forEach((span) => {
+    span.style.transform = `translate(${speedSlow}px)`;
+  });
+  spansFast.forEach((span) => {
+    span.style.transform = `translate(${speedFast}px)`
+  })
+}
+//we need to recalculate width when the window is resized
+function handleWindowResize() {
+  width = window.innerWidth;
+}
